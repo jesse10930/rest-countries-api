658c70676627f5cbceffc3a6034b49c565e921a7
jesse10930 <ronda.jesse@gmail.com>
2021-03-13 09:54:17 -0500

    styling country cards

--- dist/js/cardList.js
+++ dist/js/cardList.js
@@ -17,7 +17,24 @@ class CardList extends React.Component {
       <div id='card-list'>
         {this.state.countries
           ? this.state.countries.map((country, i) => (
-              <div className='temp'>{country.name}</div>
+              <div id='country-card'>
+                <img src={country.flag} />
+                <div id='description'>
+                  <h2 className='line'>{country.name}</h2>
+                  <p className='line'>
+                    <span className='bold'>Region: </span>
+                    {country.region}
+                  </p>
+                  <p className='line'>
+                    <span className='bold'>Population: </span>
+                    {country.population}
+                  </p>
+                  <p className='line'>
+                    <span className='bold'>Capital: </span>
+                    {country.capital}
+                  </p>
+                </div>
+              </div>
             ))
           : 'JSON.stringify(this.state.countries)'}
       </div>
